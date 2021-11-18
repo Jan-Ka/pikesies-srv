9a0e1ed40127fe1e48f217977abc0ad7566d3620
Jan Kaltenecker <jan-ka@users.noreply.github.com>
2021-11-18 22:12:35 +0100

    deduplicate code
    cleanup code in server.go

--- server/server.go
+++ server/server.go
@@ -12,36 +12,10 @@ import (
 )
 
 func RunSingleHandlerServer(ctx context.Context, wg *sync.WaitGroup, addr string, pattern string, handler handlers.PikesiesHandler) {
-	serverLog := log.With().Str("addr", addr).Str("pattern", pattern).Logger()
+	r := mux.NewRouter()
+	r.HandleFunc(pattern, handler)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc(pattern, handler)
-	server := &http.Server{
-		Addr:    addr,
-		Handler: mux,
-	}
-
-	go func() {
-		<-ctx.Done()
-
-		shutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		if err := server.Shutdown(shutCtx); err != nil {
-			log.Error().Msgf("error shutting down handler %s\n", err)
-		}
-
-		serverLog.Info().Msg("handler is now closed")
-		wg.Done()
-	}()
-
-	serverLog.Info().Msg("handler is starting")
-
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		serverLog.Error().Msgf("error starting up handler %s\n", err)
-	}
-
-	serverLog.Info().Msg("handler is closing")
+	RunServer(ctx, wg, addr, r)
 }
 
 func RunServer(ctx context.Context, wg *sync.WaitGroup, addr string, router *mux.Router) {
