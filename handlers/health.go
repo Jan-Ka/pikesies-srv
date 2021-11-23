c5f877866b639fb8ae52e7d30e468871779383dd
Jan Kaltenecker <jan-ka@users.noreply.github.com>
2021-11-24 00:21:42 +0100

    log http code for requests in handlers

--- handlers/health.go
+++ handlers/health.go
@@ -3,9 +3,16 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 func HealthHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.WriteHeader(http.StatusOK)
-	fmt.Fprint(rw, http.StatusText(http.StatusOK))
+	handlerLog := log.With().Str("package", "handlers").Str("handler", "health").Logger()
+
+	s := http.StatusOK
+	rw.WriteHeader(s)
+	st := http.StatusText(s)
+	handlerLog.Debug().Int("code", s).Msg(st)
+	fmt.Fprint(rw, st)
 }
--- handlers/retrievecss.go
+++ handlers/retrievecss.go
@@ -10,8 +10,9 @@ import (
 func RetrieveCSSHandler(rw http.ResponseWriter, r *http.Request) {
 	handlerLog := log.With().Str("package", "handlers").Str("handler", "retrieveCss").Logger()
 
-	rw.WriteHeader(http.StatusOK)
+	s := http.StatusOK
+	rw.WriteHeader(s)
 
-	handlerLog.Debug().Msg("Thanks for testing retrieve-css!")
+	handlerLog.Debug().Int("code", s).Msg("Thanks for testing retrieve-css!")
 	fmt.Fprint(rw, "Thanks for testing retrieve-css!")
 }
